cdc5a83b53ad12c40f42acc69a1c92be4321a314
Simeon199 <sim.ki@outlook.de>
2025-04-28 21:51:57 +0200

    firebase connection established

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirebaseApp, initializeApp, FirebaseApp } from '@angular/fire/app';
 import { provideDatabase, getDatabase } from '@angular/fire/database';
 import { firebaseConfig } from './environments/firebase_config';
 
@@ -10,7 +10,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideDatabase(() => getDatabase()),
+    provideFirebaseApp(() => {
+      return initializeApp(firebaseConfig);
+    }),
+    provideDatabase(() => {
+      const app = inject(FirebaseApp);
+      return getDatabase(app);
+    }),
   ]
 };
--- src/app/environments/firebase_config.ts
+++ src/app/environments/firebase_config.ts
@@ -1,8 +1,18 @@
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyAyeBznRT-zHCp2wccNzHGH6YEbHNVZtsw",
+//     authDomain: "join-angular-3a3f2.firebaseapp.com",
+//     projectId: "join-angular-3a3f2",
+//     storageBucket: "join-angular-3a3f2.firebasestorage.app",
+//     messagingSenderId: "12048006646",
+//     appId: "1:12048006646:web:e96b513a6fb1646be09bd7"
+//   };
+
 export const firebaseConfig = {
-    apiKey: "AIzaSyAyeBznRT-zHCp2wccNzHGH6YEbHNVZtsw",
-    authDomain: "join-angular-3a3f2.firebaseapp.com",
-    projectId: "join-angular-3a3f2",
-    storageBucket: "join-angular-3a3f2.firebasestorage.app",
-    messagingSenderId: "12048006646",
-    appId: "1:12048006646:web:e96b513a6fb1646be09bd7"
-  };
+  apiKey: "AIzaSyAyeBznRT-zHCp2wccNzHGH6YEbHNVZtsw",
+  authDomain: "join-angular-3a3f2.firebaseapp.com",
+  databaseURL: "https://join-angular-3a3f2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "join-angular-3a3f2",
+  storageBucket: "join-angular-3a3f2.firebasestorage.app",
+  messagingSenderId: "12048006646",
+  appId: "1:12048006646:web:e96b513a6fb1646be09bd7"
+};
