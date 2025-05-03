f2ebf72e4865d10fb3adbab8649da1a31a5cc9e5
Simeon199 <sim.ki@outlook.de>
2025-05-03 22:25:32 +0200

    adding reactivity to register component and register form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterLink } from '@angular/router';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   imports: [
     CommonModule,
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-
+  registerForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    passwordRepeated: new FormControl('')
+});
 }
