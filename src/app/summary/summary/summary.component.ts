2f63f13aedaa076944f2f4ebc3f1d5b27824da26
Simeon199 <sim.ki@outlook.de>
2025-05-07 14:31:56 +0200

    some basic styling added to header and sidebar

--- src/app/summary/summary/summary.component.ts
+++ src/app/summary/summary/summary.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-summary',
-  imports: [SidebarComponent],
+  imports: [
+    SidebarComponent,
+    HeaderComponent
+  ],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
