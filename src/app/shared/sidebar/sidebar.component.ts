55bc1a3af5990af42afc250f3769ae5b25611050
Simeon199 <sim.ki@outlook.de>
2025-05-08 20:39:08 +0200

    background highlighting for sidebar links added

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   imports: [
-    RouterLink
+    RouterLink,
+    NgClass
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
 
+export class SidebarComponent {
+  currentLocation: string =  '';
+
+  constructor(private router: Router){
+    this.currentLocation = this.router.url;
+    this.returnCurrentLocation();
+  }
+
+  returnCurrentLocation(){
+    console.log('return current location: ', this.currentLocation);
+  }
+
+  highlightLink(path: string){
+    if(this.doesPathCoincide(path)){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  doesPathCoincide(path: string){
+    return path === this.currentLocation;
+  }
 }
