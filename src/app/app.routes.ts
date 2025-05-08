09c6b9e3a09c205531860d9093564e427bf13fdd
Simeon199 <sim.ki@outlook.de>
2025-05-08 21:59:27 +0200

    remaining components added to application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { SummaryComponent } from './summary/summary.component';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { BoardComponent } from './board/board.component';
 import { ContactComponent } from './contact/contact.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { HelpSectionComponent } from './help-section/help-section.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
@@ -13,5 +16,8 @@ export const routes: Routes = [
     {path: 'summary', component: SummaryComponent},
     {path: 'add-task', component: AddTaskComponent},
     {path: 'board', component: BoardComponent},
-    {path: 'contact', component: ContactComponent}
+    {path: 'contact', component: ContactComponent},
+    {path: 'privacy-policy', component: PrivacyPolicyComponent},
+    {path: 'legal-notice', component: LegalNoticeComponent},
+    {path: 'help-section', component: HelpSectionComponent}
 ];
---
+++ src/app/help-section/help-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-section',
+  imports: [],
+  templateUrl: './help-section.component.html',
+  styleUrl: './help-section.component.scss'
+})
+export class HelpSectionComponent {
+
+}
