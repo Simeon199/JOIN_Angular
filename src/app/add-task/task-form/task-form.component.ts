3db6f362094d86e05f9fa872229b0d08376baf13
Simeon199 <sim.ki@outlook.de>
2025-08-15 16:30:43 +0200

    some changes to task-form.component added

--- src/app/add-task/task-form/task-form.component.ts
+++ src/app/add-task/task-form/task-form.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.scss'
 })
 export class TaskFormComponent {
-
+  addTaskForm = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+    assignedTo: new FormControl(''),
+    date: new FormControl(''),
+    prio: new FormControl(''),
+    category: new FormControl(''),
+    subtasks: new FormControl('')
+  });
 }
