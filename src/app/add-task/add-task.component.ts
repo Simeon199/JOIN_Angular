81cb5b4c4965bf168aa68d66739bf4ecfa9b7b61
Simeon199 <sim.ki@outlook.de>
2025-08-15 15:32:05 +0200

    add-task form wrapper added and new component created

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from '../shared/components/sidebar/sidebar.component';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { DropdownComponent } from '../shared/components/dropdown/dropdown.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 @Component({
   selector: 'app-add-task',
   imports: [
     SidebarComponent,
     HeaderComponent,
-    DropdownComponent
+    DropdownComponent,
+    TaskFormComponent
   ],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
