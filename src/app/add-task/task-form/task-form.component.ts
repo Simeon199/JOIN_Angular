055f8f9817c3a62d4f476727b5e758a4a457949f
Simeon199 <sim.ki@outlook.de>
2025-08-18 15:40:35 +0200

    feat: selecting items from dropdown in input field enabled

--- src/app/add-task/task-form/task-form.component.ts
+++ src/app/add-task/task-form/task-form.component.ts
@@ -10,6 +10,15 @@ import { CommonModule } from '@angular/common';
   styleUrl: './task-form.component.scss'
 })
 export class TaskFormComponent {
+
+  showContactsDropdown:boolean = false;
+  showTaskCategoryDropdown:boolean = false;
+  selectedTaskCategory:string = '';
+  selectedContact:string = '';
+  contacts:Array<string> = ['Mark', 'Anton'];
+  taskCategories:Array<string> = ['Technical Task', 'User Story'];
+  filteredContacts = this.contacts;
+
   addTaskForm = new FormGroup({
     title: new FormControl(''),
     description: new FormControl(''),
@@ -19,4 +28,29 @@ export class TaskFormComponent {
     category: new FormControl(''),
     subtasks: new FormControl('')
   });
+
+  onInput(event: Event){
+    let value = (event.target as HTMLInputElement).value;
+    this.filteredContacts = this.contacts.filter(contact => 
+      contact.toLowerCase().includes(value.toLowerCase())
+    );
+  }
+
+  selectContact(contact: string){
+    this.selectedContact = contact;
+    this.showContactsDropdown = false;
+  }
+
+  selectTaskCategory(taskCategory: string){
+    this.selectedTaskCategory = taskCategory;
+    this.showTaskCategoryDropdown = false;
+  }
+
+  hideContactsDropdown(){
+    setTimeout(() => this.showContactsDropdown = false, 200) 
+  }
+
+  hideTaskCategoryDropdown(){
+    setTimeout(() => this.showTaskCategoryDropdown = false, 200)
+  }
 }
